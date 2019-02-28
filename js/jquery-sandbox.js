d09b5e9ba8299fac27cd78f819449c23be95cbc9
Carlo Munguia <carlomunguia@carlos-air.home>
2019-02-28 10:32:25 -0600

    a few day push of different work for class & otherwise

---
+++ js/jquery-sandbox.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    console.log(document.getElementById('hello').innerHTML);
+
+console.log(jQuery);
+console.log(typeof jQuery);
+});
--- practice.js
+++ practice.js
@@ -1,1 +1,199 @@
-console.log (hrl)
+------------- PRACTICE 1
+
+Write a function, filterNumbers() that takes in an array of mixed data types and returns an array of only the numbers type in ascending order.
+
+    Example input: ["fred", true, 5, 3]
+Example output: [3, 5]
+
+
+function filterNumbers(array){
+
+    let result = []
+    for(let i = 0; i < array.length; i++){
+        const item = array[i]
+        if(typeof array[i] === “number”){
+            result.push(item);
+        }
+    }
+    result.sort()
+    return result
+
+    // return array.filter(item => !isNaN(item)),sort();
+}
+
+var names = ['hello', 'world']
+
+
+------------- PRACTICE 2
+
+Write a function, getOlder() that takes in array of dog objects and increases the value of the age properties by 1.
+
+
+function getOlder(array){
+    let result = [name, breed, age]
+    for(let i = 0; i < array.length; i += 1){
+        const item = array[i]
+
+    }
+}
+Example input: [
+    {
+        name: "Chompers",
+        breed: "Pug",
+        age: 7
+    },
+    {
+        name: "Freddy",
+        breed: "Lab",
+        age: 4
+    },
+    {
+        name: "Mr. Pig",
+        breed: "Mastif",
+        age: 10
+    }
+];
+
+Example output: [
+    {
+        name: "Chompers",
+        breed: "Pug",
+        age: 8
+    },
+    {
+        name: "Freddy",
+        breed: "Lab",
+        age: 5
+    },
+    {
+        name: "Mr. Pig",
+        breed: "Mastif",
+        age: 11
+    }
+];
+
+------------- PRACTICE 3
+
+Write a function, washCars() that takes in a array of car objects and sets the boolean properties of isDirty to false
+
+Example input: [
+    {
+        make: 'Volvo',
+        color: 'red',
+        year: 1996,
+        isDirty: true
+    },
+    {
+        make: 'Toyota',
+        color: 'black',
+        year: 2004,
+        isDirty: false
+    },
+    {
+        make: 'Ford',
+        color: 'white',
+        year: 2007,
+        isDirty: true
+    }
+]
+
+Example output:
+    [
+        {
+            make: 'Volvo',
+            color: 'red',
+            year: 1996,
+            isDirty: false // changed to false
+        },
+        {
+            make: 'Toyota',
+            color: 'black',
+            year: 2004,
+            isDirty: false // stays the same
+        },
+        {
+            make: 'Ford',
+            color: 'white',
+            year: 2007,
+            isDirty: false // changed to false
+        }
+    ]
+
+------------- PRACTICE 4
+
+Write a function, adminList() that takes in an array of user objects and returns a count of all admins based on the isAdmin property. Refactor to return an array of admin-only user emails. Refactor again to return an array of user objects that are admins.
+
+    Example Input: [
+    {
+        isAdmin: true,
+        email: 'user1@email.com'
+    },
+    {
+        isAdmin: true,
+        email: 'user2@email.com'
+    }
+    {
+        isAdmin: false,
+        email: 'user3@email.com'
+    }
+];
+
+Example Output (before refactor): 2
+
+Example Output (after 1st refactor): [
+    'user1@email.com',
+    'user2@email.com'
+]
+
+Example Output (after 2nd refactor): [
+    {
+        isAdmin: true,
+        email: 'user1@email.com'
+    },
+    {
+        isAdmin: true,
+        email: 'user2@email.com'
+    }
+]
+
+------------- PRACTICE 5
+
+Create a function, makeSandwhichObjects() that takes in two array of strings, breads and fillings and returns an array of sandwhichObjects that contain properties for bread and filling and values correspond to the same order of the two passed in arrays. Assume the two array inputs are the same length.
+
+    Example Input:
+
+    var breads  = [
+        "white",
+        "wheat",
+        "rye",
+        "white"
+    ];
+
+var fillings = [
+    "pb&j",
+    "ham",
+    "cheese steak",
+    "tuna"
+];
+
+makeSandwhichObjects(breads, fillings) // example call to the function
+
+Example Output: [
+    {
+        bread: "white,
+        filling: "pb&j"
+    },
+    {
+        bread: "wheat",
+        filling: "ham"
+    },
+    {
+        bread: "rye",
+        filling: "cheese steak"
+    },
+    {
+        bread: "white",
+        filling: "tuna"
+    }
+]
+
